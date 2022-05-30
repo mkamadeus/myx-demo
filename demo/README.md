0cf6da623e32ac3e3a7ae02c99cd3e028ca49eb8
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2022-05-30 17:22:25 +0700

    feat: initial prep

---
+++ demo/README.md
@@ -0,0 +1,10 @@
+# `examples` directory
+
+Holds examples of code generation using this tool.
+Examples include:
+
+- `scikit-learn` based tabular model
+- `keras` based convolutional neural network model
+
+More examples will be added in work.
+Some files will not be included in the repository since it has quite a large size.
---
+++ demo/clean.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# simple script to reset directory to its original form
+
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+projects=("titanic" "mnist" "churn" "catdog")
+
+for d in ${projects[@]}; do
+	echo -e "\e[33mremoving files for project [$d]\e[0m"
+	pushd "./$d"
+		rm -f .dockerignore .gitignore Dockerfile requirements.txt main.py
+	popd
+done
