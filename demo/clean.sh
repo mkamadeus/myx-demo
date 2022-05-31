a18512823e4a37cfb4ba710f4da872f8a180baa7
Matthew Kevin Amadeus <40513202+mkamadeus@users.noreply.github.com>
2022-05-31 15:44:45 +0700

    feat: venv deletion clean.sh

--- demo/clean.sh
+++ demo/clean.sh
@@ -16,5 +16,6 @@ for d in ${projects[@]}; do
 	echo -e "\e[33mremoving files for project [$d]\e[0m"
 	pushd "./$d"
 		rm -f .dockerignore .gitignore Dockerfile requirements.txt main.py
+		rm -rf venv
 	popd
 done
